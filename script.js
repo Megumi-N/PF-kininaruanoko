14f553c97aa27216980e04a4715d66cf21922de4
noguchi <02125011a@gmail.com>
2021-10-10 19:25:11 +0900

    fix: windowを開いたときに許可が出るように設定

--- script.js
+++ script.js
@@ -204,9 +204,10 @@ function init() {
   os = detectOSSimply();
   if (os == "iphone") {
     // safari用。DeviceOrientation APIの使用をユーザに許可して貰う
-    document
-      .querySelector("#permit")
-      .addEventListener("click", permitDeviceOrientationForSafari);
+    permitDeviceOrientationForSafari();
+    // document
+    //   .querySelector("#permit")
+    //   .addEventListener("click", permitDeviceOrientationForSafari);
     //  加速度センサーの値を取得
     window.addEventListener(
       "deviceorientation",
